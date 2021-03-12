170e981e9e4b32251f48e0f833098f5971925da6
vengateshl <62301944+vengateshl@users.noreply.github.com>
2021-03-12 12:16:31 +0530

    Update hello.java

--- firstcheck/src/firstcheck/hello.java
+++ firstcheck/src/firstcheck/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello111");
+System.out.println("hello112");
 System.out.println("hi");
 	}
 
